871a9f5e4e75cb4a7cd3749ca8733b093d7be0d2
codershisho <codershisho@gmail.com>
2023-09-24 15:35:51 +0900

    pre vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: [
+    "~/assets/css/main.css",
+    "vuetify/lib/styles/main.sass",
+    "mdi/css/materialdesignicons.min.css",
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
     },
   },
 });
