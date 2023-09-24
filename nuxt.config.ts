c96ab201921ee6a63b97e3df9e5e68e3d0a7cdf1
codershisho <codershisho@gmail.com>
2023-09-24 16:28:09 +0900

    init pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/",
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap",
+          crossorigin: "",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   css: [
     "~/assets/css/main.css",
@@ -16,8 +32,18 @@ export default defineNuxtConfig({
     transpile: ["vuetify"],
   },
   vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
     define: {
       "process.env.DEBUG": false,
     },
+  },
+  typescript: {
+    shim: false, // shimsファイル生成の無効化（VSCodeでVolarを使う場合はfalseにする）
+    strict: true, // 型チェックを厳格化
+    typeCheck: false, // nuxt devまたはnuxt build時に型チェックを実行
   },
 });
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="font-bold">abbb</div>
+  <v-btn color="primary">aaa</v-btn>
+</template>
+
+<script setup lang="ts">
+import { VBtn } from "vuetify/lib/components/index.mjs";
+</script>
